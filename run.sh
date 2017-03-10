08ef53bedee2ac13cbc87085e7f5acb22ca729d5
jma <jacky.ma@affinitas.de>
2017-03-10 11:38:47 +0100

    completed docker pipeline

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 if [ "$1" == "-b" ];then
   echo "Build Image: yelpanalysis"
-  sudo docker build -t yelpanalysis .
+  docker build -t yelpanalysis .
 fi
 
-sudo docker images
-sudo docker rm -f yelp-dev
-sudo docker run -td -p 8888:8888 -p 27017:27017 --name yelp-dev -v `pwd`/data:/datamount yelpanalysis mongod --bind_ip=0.0.0.0
+docker images
+docker rm -f yelp-dev
+docker run -td -p 8888:8888 -p 27017:27017 --name yelp-dev -v `pwd`/data:/datamount yelpanalysis mongod --bind_ip=0.0.0.0
 
 if [ -f "data/yelp_academic_dataset_business.json" ] &&
    [ -f "data/yelp_academic_dataset_checkin.json" ] &&
@@ -15,22 +15,30 @@ if [ -f "data/yelp_academic_dataset_business.json" ] &&
    [ -f "data/yelp_academic_dataset_tip.json" ] &&
    [ -f "data/yelp_academic_dataset_user.json" ]; then
    echo "json files exist. untar skipped."
-   until nc -z $(sudo docker inspect --format'{{.NetworkSettings.IPAddress}}' yelp-dev) 27017
-   do
-      echo "waiting for mongod..."
-      sleep 5
-   done
+
+   attempt=0
+   while [ $attempt -le 59 ]; do
+      attempt=$(( $attempt + 1 ))
+      echo "Waiting for server to be up (attempt: $attempt)..."
+      result=$(docker logs yelp-dev)
+      if grep -q 'waiting for connections on port 27017' <<< $result ; then
+         echo "Mongodb is up!"
+         break
+      fi
+   sleep 5
+done
+
 else
    echo "untar compressed file."
-   sudo docker exec yelp-dev tar xvzf /datamount/yelp_dataset_challenge_round9.tar -C /datamount
+   docker exec yelp-dev tar xvzf /datamount/yelp_dataset_challenge_round9.tar -C /datamount
 fi
 
-sudo docker exec yelp-dev mongoimport --db yelp --collection business /datamount/yelp_academic_dataset_business.json
-sudo docker exec yelp-dev mongoimport --db yelp --collection checkin /datamount/yelp_academic_dataset_checkin.json
-sudo docker exec yelp-dev mongoimport --db yelp --collection review /datamount/yelp_academic_dataset_review.json
-sudo docker exec yelp-dev mongoimport --db yelp --collection tip /datamount/yelp_academic_dataset_tip.json
-sudo docker exec yelp-dev mongoimport --db yelp --collection user /datamount/yelp_academic_dataset_user.json
+docker exec yelp-dev mongoimport --db yelp --collection business /datamount/yelp_academic_dataset_business.json
+docker exec yelp-dev mongoimport --db yelp --collection checkin /datamount/yelp_academic_dataset_checkin.json
+docker exec yelp-dev mongoimport --db yelp --collection review /datamount/yelp_academic_dataset_review.json
+docker exec yelp-dev mongoimport --db yelp --collection tip /datamount/yelp_academic_dataset_tip.json
+docker exec yelp-dev mongoimport --db yelp --collection user /datamount/yelp_academic_dataset_user.json
 
-sudo docker exec yelp-dev python /yelpanalysis/analysis.py
-sudo docker exec -i -t yelp-dev /bin/bash
+docker exec yelp-dev python /yelpanalysis/analysis.py
+docker exec -i -t yelp-dev /bin/bash
 
